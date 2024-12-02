c426cff02b6cbd6a4250357125fe2962be02b990
Jarvis Choy <choyja2@oocl.com>
2024-12-02 11:30:56 +0800

    refactor: fix base url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-  baseURL: "https://1617495bca868020296630a5b2.mockapi.io/todoList",
+  baseURL: "https://67495bca868020296630a5b2.mockapi.io/todoList",
 })
 
 client.interceptors.request.use(
