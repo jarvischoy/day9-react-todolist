04e401ca93ccceeb48af8b59e55688c0b263add5
Jarvis Choy <choyja2@oocl.com>
2024-12-02 10:02:14 +0800

    feat: add loading mask for modal when confirm update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,7 +4,7 @@ import { TodoContext } from "../context/TodoContext"
 import { ActionEnum } from "../enum/ActionEnum"
 import styles from "./TodoItem.module.css"
 import { deleteTodos, updateTodos } from "../api/todos"
-import { Button, Input, Modal, Spin } from "antd"
+import { Button, Col, Input, Modal, Row, Space, Spin } from "antd"
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons"
 
 const TodoItem = ({ todo }) => {
@@ -12,7 +12,6 @@ const TodoItem = ({ todo }) => {
   const { dispatch } = useContext(TodoContext)
   const [isLoading, setIsLoading] = useState(false)
   const [isShowUpdateModal, setIsShowUpdateModal] = useState(false)
-  // const [originalText, setOriginalText] = useState(text)
   const [updateText, setUpdateText] = useState(text)
 
   const handleDelete = async () => {
@@ -42,7 +41,6 @@ const TodoItem = ({ todo }) => {
       .catch((error) => console.error("Failed to update todo", error))
       .finally(() => {
         setIsLoading(false)
-        // setOriginalText(updateText)
         toggleUpdateModal()
       })
 
@@ -69,8 +67,16 @@ const TodoItem = ({ todo }) => {
       </div>
     </Spin>
 
-    <Modal title="Update Todo" open={isShowUpdateModal} onOk={handleOk} onCancel={handleCancel} >
-      <Input value={updateText} onChange={handleUpdateTextChange} />
+    <Modal title="Update Todo" open={isShowUpdateModal} onCancel={handleCancel} footer={null}>
+      <Spin spinning={isLoading}>
+        <Col >
+          <Input value={updateText} onChange={handleUpdateTextChange} />
+          <Row justify={"end"} style={{ marginTop: "10px", gap: "10px" }}>
+            <Button color="primary" variant="solid" onClick={handleOk}>Update</Button>
+            <Button color="danger" variant="solid" onClick={handleCancel}>Cancel</Button>
+          </Row>
+        </Col>
+      </Spin>
     </Modal>
   </>
 }
